755a7b683cc95c2291a1b5c5b0853aaa23e85cfd
yjkim <yjkimdj@gmail.com>
2024-05-23 09:48:50 +0900

    feat: 최종 업데이트 코드 수정
    테스트 업로드

--- app/src/main/java/kr/djspi/pipe01/Intro.kt
+++ app/src/main/java/kr/djspi/pipe01/Intro.kt
@@ -1,10 +1,13 @@
 package kr.djspi.pipe01
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Build
 import android.os.Bundle
 import android.os.Handler
+import android.os.Looper
 import android.util.Log
+import android.widget.TextView
 import android.widget.Toast
 import androidx.activity.result.ActivityResult
 import androidx.activity.result.contract.ActivityResultContracts.*
@@ -24,7 +27,6 @@ import kotlin.system.exitProcess
 
 class Intro : AppCompatActivity() {
 
-    private lateinit var introBinding: ActivityIntroBinding
     private lateinit var appUpdateManager: AppUpdateManager
     private var listener: InstallStateUpdatedListener? = null
 
@@ -36,8 +38,7 @@ class Intro : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         Log.d("SPI", "IntroActivity\nonCreate() start")
 
-        introBinding = ActivityIntroBinding.inflate(layoutInflater)
-        setContentView(introBinding.root)
+        setContentView(R.layout.activity_intro)
 
         if (BuildConfig.BUILD_TYPE == "release") {
             val installer = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
@@ -51,52 +52,10 @@ class Intro : AppCompatActivity() {
                 System.runFinalization()
                 exitProcess(0)
             }
+            // ref: 인앱 업데이트 지원 (https://developer.android.com/guide/playcore/in-app-updates/kotlin-java?hl=ko)
+            appUpdateManager = AppUpdateManagerFactory.create(this)
         }
-
-        // ref: 인앱 업데이트 지원 (https://developer.android.com/guide/playcore/in-app-updates/kotlin-java?hl=ko)
-        appUpdateManager = AppUpdateManagerFactory.create(this)
-
-        // Create a listener to track request state updates.
-        listener = InstallStateUpdatedListener { state ->
-            // (Optional) Provide a download progress bar.
-            if (state.installStatus() == InstallStatus.DOWNLOADING) {
-                val megabytesDownloaded =
-                    ((state.bytesDownloaded() / 1e6) * 10.0).roundToLong() / 10.0
-                val totalMegabytesToDownload =
-                    ((state.totalBytesToDownload() / 1e6) * 10.0).roundToLong() / 10.0
-                val percentage =
-                    (state.bytesDownloaded() * 100 / state.totalBytesToDownload()).toString()
-                val progress =
-                    "업데이트 중입니다...\n$megabytesDownloaded / $totalMegabytesToDownload ($percentage %)"
-                introBinding.updateText.text = progress
-                Log.d("SPI", "IntroActivity\nmegabytesDownloaded: $megabytesDownloaded")
-                Log.d("SPI", "IntroActivity\ntotalMegabytesToDownload: $totalMegabytesToDownload")
-                Log.d("SPI", "IntroActivity\npercentage: $percentage")
-                Log.d("SPI", "IntroActivity\nprogress: $progress")
-            } else if (state.installStatus() == InstallStatus.DOWNLOADED) {
-                popupToastForCompleteUpdate()
-            }
-            // Log state or install the update.
-        }
-
         checkUpdate()
-    }
-
-    // Checks that the update is not stalled during 'onResume()'.
-    // However, you should execute this check at all entry points into the app.
-    override fun onResume() {
-        super.onResume()
-        appUpdateManager.appUpdateInfo.addOnSuccessListener { appUpdateInfo ->
-            if (appUpdateInfo.updateAvailability() == UpdateAvailability.DEVELOPER_TRIGGERED_UPDATE_IN_PROGRESS) {
-                // If an in-app update is already running, resume the update.
-                Log.d("SPI", "IntroActivity\nonResume: Developer triggered update in progress")
-                appUpdateInfo.immediateUpdate()
-            }
-            if (appUpdateInfo.installStatus() == InstallStatus.DOWNLOADED) {
-                Log.d("SPI", "IntroActivity\nonResume: Downloaded")
-                popupToastForCompleteUpdate()
-            }
-        }
     }
 
     override fun onDestroy() {
@@ -111,8 +70,10 @@ class Intro : AppCompatActivity() {
 
     private fun runMainActivity() {
         Log.d("SPI", "IntroActivity\nrunMainActivity() start")
-        Handler().postDelayed({
-            startActivity(Intent(baseContext, MainActivity::class.java))
+        Handler(
+            Looper.getMainLooper()
+        ).postDelayed({
+            startActivity(Intent(this, MainActivity::class.java))
             finish()
         }, 750)
     }
@@ -120,46 +81,53 @@ class Intro : AppCompatActivity() {
     private fun checkUpdate() {
         Log.d("SPI", "IntroActivity\ncheckUpdate() start")
         // Checks that the platform will allow the specified type of update.
-        appUpdateManager.appUpdateInfo.addOnSuccessListener { appUpdateInfo ->
-            Log.d("SPI", "IntroActivity\nappUpdateInfoTask success listener")
-            when (appUpdateInfo.updateAvailability()) {
-                UpdateAvailability.UPDATE_AVAILABLE -> {
-                    Log.d("SPI", "IntroActivity\ncheckUpdate: Update available")
-                    when {
-                        // This example applies an immediate update.
-                        appUpdateInfo.isUpdateTypeAllowed(AppUpdateType.IMMEDIATE) -> {
-                            appUpdateInfo.immediateUpdate()
+        try {
+            appUpdateManager.appUpdateInfo.addOnSuccessListener { appUpdateInfo ->
+                Log.d("SPI", "IntroActivity\nappUpdateInfoTask success listener")
+                when (appUpdateInfo.updateAvailability()) {
+                    UpdateAvailability.UPDATE_AVAILABLE -> {
+                        Log.d("SPI", "IntroActivity\ncheckUpdate: Update available")
+                        when {
+                            // This example applies an immediate update.
+                            appUpdateInfo.isUpdateTypeAllowed(AppUpdateType.IMMEDIATE) -> {
+                                appUpdateInfo.immediateUpdate()
+                            }
+                            // To apply a flexible update instead, pass in AppUpdateType.FLEXIBLE
+                            appUpdateInfo.isUpdateTypeAllowed(AppUpdateType.FLEXIBLE) -> {
+                                appUpdateInfo.flexibleUpdate()
+                            }
                         }
-                        // To apply a flexible update instead, pass in AppUpdateType.FLEXIBLE
-                        appUpdateInfo.isUpdateTypeAllowed(AppUpdateType.FLEXIBLE) -> {
-                            appUpdateInfo.flexibleUpdate()
-                        }
+                    }
+
+                    UpdateAvailability.DEVELOPER_TRIGGERED_UPDATE_IN_PROGRESS -> {
+                        // If an in-app update is already running, resume the update.
+                        appUpdateInfo.immediateUpdate()
+                    }
+
+                    else -> {
+                        Log.d("SPI", "IntroActivity\nUpdate not available")
+                        runMainActivity()
                     }
                 }
 
-                UpdateAvailability.DEVELOPER_TRIGGERED_UPDATE_IN_PROGRESS -> {
-                    // If an in-app update is already running, resume the update.
-                    appUpdateInfo.immediateUpdate()
-                }
-
-                else -> {
-                    Log.d("SPI", "IntroActivity\nUpdate not available")
-                    runMainActivity()
+                if (appUpdateInfo.installStatus() == InstallStatus.DOWNLOADED) {
+                    Log.d("SPI", "IntroActivity\nonResume: Downloaded")
+                    popupToastForCompleteUpdate()
                 }
             }
+        } catch (e: Exception) {
+            runMainActivity()
         }
     }
 
     private val activityResultLauncher =
         registerForActivityResult(StartIntentSenderForResult()) { result: ActivityResult ->
             // handle callback
-            when (result.resultCode) {
-                RESULT_OK -> runMainActivity()
+            if (result.resultCode != RESULT_OK) {
+                Log.d("SPI", "IntroActivity\nResult cancel or fail")
+                Toast.makeText(this, "플레이스토어에서 앱을 업데이트해주세요.", Toast.LENGTH_LONG).show()
                 // If the update is canceled or fails, you can request to start the update again.
-                else -> {
-                    Log.d("SPI", "IntroActivity\nResult cancel or fail")
-                    Toast.makeText(this, "플레이스토어에서 앱을 업데이트해주세요.", Toast.LENGTH_LONG).show()
-                }
+                runMainActivity()
             }
         }
 
@@ -174,9 +142,39 @@ class Intro : AppCompatActivity() {
         )
     }
 
+    @SuppressLint("SwitchIntDef")
     private fun AppUpdateInfo.immediateUpdate() {
         Log.d("SPI", "IntroActivity\ncheckUpdate: Immediate update")
+
+        // Create a listener to track request state updates.
+        listener = InstallStateUpdatedListener { state ->
+            // (Optional) Provide a download progress bar.
+            when (state.installStatus()) {
+                InstallStatus.DOWNLOADING -> {
+                    val megabytesDownloaded =
+                        ((state.bytesDownloaded() / 1e6) * 10.0).roundToLong() / 10.0
+                    val totalMegabytesToDownload =
+                        ((state.totalBytesToDownload() / 1e6) * 10.0).roundToLong() / 10.0
+                    val percentage =
+                        (state.bytesDownloaded() * 100 / state.totalBytesToDownload()).toString()
+                    val progress =
+                        "업데이트 중입니다...\n$megabytesDownloaded / $totalMegabytesToDownload ($percentage %)"
+                    findViewById<TextView>(R.id.update_text).text = progress
+                    Log.d("SPI", "IntroActivity\nmegabytesDownloaded: $megabytesDownloaded")
+                    Log.d("SPI", "IntroActivity\ntotalMegabytesToDownload: $totalMegabytesToDownload")
+                    Log.d("SPI", "IntroActivity\npercentage: $percentage")
+                    Log.d("SPI", "IntroActivity\nprogress: $progress")
+                }
+
+                InstallStatus.DOWNLOADED -> {
+                    popupToastForCompleteUpdate()
+                }
+            }
+            // Log state or install the update.
+        }
+
         // Request the update.
+        appUpdateManager.registerListener(listener!!)
         appUpdateManager.startUpdateFlowForResult(
             // Pass the intent that is returned by 'getAppUpdateInfo()'.
             this,
