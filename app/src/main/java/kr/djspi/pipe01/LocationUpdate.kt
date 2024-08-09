065f66d509c0c5c1b0e89269b3bd7aebac429563
yjkim <yjkimdj@gmail.com>
2024-08-09 09:30:02 +0900

    fix: 앱 실행시 Permission Required 메세지 창 발생 (targetSdk 31)
    Manifest에 정의하지 않은 권한 요청 삭제

--- app/src/main/java/kr/djspi/pipe01/LocationUpdate.kt
+++ app/src/main/java/kr/djspi/pipe01/LocationUpdate.kt
@@ -63,11 +63,11 @@ abstract class LocationUpdate : BaseActivity() {
     private fun requestAllPermissions(context: Context) {
         val permissions =
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.UPSIDE_DOWN_CAKE) {
-                arrayOf(ACCESS_FINE_LOCATION, READ_MEDIA_IMAGES, READ_MEDIA_VISUAL_USER_SELECTED, CAMERA)
-            } else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
                 arrayOf(ACCESS_FINE_LOCATION, READ_MEDIA_IMAGES, CAMERA)
+            } else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+                arrayOf(ACCESS_FINE_LOCATION, ACCESS_COARSE_LOCATION, READ_MEDIA_IMAGES, CAMERA)
             } else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
-                arrayOf(ACCESS_FINE_LOCATION, READ_EXTERNAL_STORAGE, ACCESS_MEDIA_LOCATION, CAMERA)
+                arrayOf(ACCESS_FINE_LOCATION, READ_EXTERNAL_STORAGE, CAMERA)
             } else {
                 arrayOf(ACCESS_FINE_LOCATION, WRITE_EXTERNAL_STORAGE, READ_EXTERNAL_STORAGE, CAMERA)
             }
